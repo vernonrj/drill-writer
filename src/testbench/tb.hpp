b8054c923cf00e5b719f8e730b4bc59dc215ba2d
Vernon Jones <vernonrjones@gmail.com>
2012-04-08 16:13:08 -0700

    Added tb iface for sets

--- src/testbench/tb.hpp
+++ src/testbench/tb.hpp
@@ -10,3 +10,11 @@ int tbSets(void);
 int tbCoords(void);
 int tbUnRedo(void);
 
+int tbPerfs_attr(void);
+int tbPerfs_add(void);
+int tbPerfs_del(void);
+
+int tbSets_attr(void);
+int tbSets_add(void);
+int tbSets_del(void);
+
--- src/testbench/testbench.cpp
+++ src/testbench/testbench.cpp
@@ -56,13 +56,103 @@ int mainmenu(void)
 int tbPerfs(void)
 {
 	// testbench for performers
+	queue *fifo;
+	int mysel;
+	int excode;
+	
+	fifo = new queue;
+
+	fifo->enqueue("Performers");
+	fifo->enqueue("Attributes...");
+	fifo->enqueue("Add");
+	fifo->enqueue("Delete");
+	fifo->enqueue("Back");
+	do
+	{
+		mysel = menu(fifo);
+		switch(mysel)
+		{
+			case 1:		// attributes
+				excode = tbPerfs_attr();
+				break;
+			case 2:		// Add
+				excode = tbPerfs_add();
+				break;
+			case 3:		// Delete
+				excode = tbPerfs_del();
+				break;
+			case 4:		// Back
+				excode = 1;
+				break;
+		}
+	} while (excode == 0);
+
+
+	delete fifo;
 	return 0;
 }
 
 int tbSets(void)
 {
+	// testbench for sets
+	queue *fifo;
+	int mysel;
+	int excode;
+	char *set_buf;
+	
+	fifo = new queue;
+
+	do
+	{
+	set_buf = new char [20];
+	sprintf(set_buf, "Set (%i)", setnum);
+	fifo->enqueue(set_buf);
+	delete [] set_buf;
+	fifo->enqueue("Attributes...");
+	fifo->enqueue("Next");
+	fifo->enqueue("Prev");
+	fifo->enqueue("First");
+	fifo->enqueue("Last");
+	fifo->enqueue("Add");
+	fifo->enqueue("Delete");
+	fifo->enqueue("Back");
+		mysel = menu(fifo);
+		switch(mysel)
+		{
+			case 1:		// attributes
+				excode = tbSets_attr();
+				break;
+			case 2:		// Next
+				set_next();
+				break;
+			case 3:		// Prev
+				set_prev();
+				break;
+			case 4:		// First
+				set_first();
+				break;
+			case 5:		// Last
+				set_last();
+				break;
+			case 6:		// Add
+				excode = tbSets_add();
+				break;
+			case 7:		// Delete
+				excode = tbSets_del();
+				break;
+			case 8:		// Back
+				excode = 1;
+				break;
+		}
+		fifo->discard();
+	} while (excode == 0);
+
+
+	delete fifo;
+	return 0;
 	return 0;
 }
+
 
 int tbCoords(void)
 {
@@ -73,3 +163,32 @@ int tbUnRedo(void)
 {
 	return 0;
 }
+
+// performers 
+int tbPerfs_attr(void)
+{
+	return 0;
+}
+int tbPerfs_add(void)
+{
+	return 0;
+}
+int tbPerfs_del(void)
+{
+	return 0;
+}
+
+
+int tbSets_attr(void)
+{
+	return 0;
+}
+int tbSets_add(void)
+{
+	return 0;
+}
+int tbSets_del(void)
+{
+	return 0;
+}
+
